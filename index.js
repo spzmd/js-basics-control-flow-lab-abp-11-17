189f7d259c80824e7cea99a5659bd09b8481af53
spzmd <b.keithdavid@gmail.com>
2017-11-18 14:32:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your code in this file!
+function scuberGreetingForFeet(string) {
+  if (string <= 400) {
+    return "This one is on me!";
+  } else if (string <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity(string) {
+  if ("NYC"==string) {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
+}
+
+function switchOnCharmFromTip(string) {
+  /*switch("thank you for everything") {
+    case 'generous' == string:
+      return "Thank you so much.";
+      break;
+    case 'not as generous' == string:
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+  }
+}
+*/
+if ("generous" == string) {
+   return "Thank you so much.";
+ } else if ("not as generous" == string) {
+   return "Thank you.";
+ } else {
+   return "Bye.";
+ }
+}
